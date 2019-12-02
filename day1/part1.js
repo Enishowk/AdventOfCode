df459741744624a6134eefcc1b7d00de804538ed
Enishowk <kenshineuh@gmail.com>
2019-12-02 18:05:13 +0100

    add editorconfig

--- day1/part1.js
+++ day1/part1.js
@@ -3,7 +3,7 @@ const content = fs.readFileSync(`${__dirname}/input.txt`, "utf8");
 
 let sum = 0;
 content.split(/\r?\n/).forEach(line => {
-  sum += Math.floor(+line / 3) - 2;
+    sum += Math.floor(+line / 3) - 2;
 });
 
 console.log(sum);
