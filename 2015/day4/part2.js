56e43ef86a5f342092eb98191af079b9cb3e505a
Enishowk <kenshineuh@gmail.com>
2019-12-06 18:44:26 +0100

    improve part 2

--- 2015/day4/part2.js
+++ 2015/day4/part2.js
@@ -2,23 +2,17 @@ const crypto = require('crypto');
 
 const input = 'ckczppom';
 
-function md5(string) {
-    const hash = crypto
+const md5 = string =>
+    crypto
         .createHash('MD5')
         .update(string)
         .digest('hex');
 
-    return hash;
-}
-
-let stop = false;
-while (!stop) {
-    for (let i = 0; i < 1e8; i += 1) {
-        const hash = md5(input + i.toString());
-        if (hash.slice(0, 6) === '000000') {
-            stop = true;
-            console.log(i);
-            break;
-        }
+let i = 0;
+while (i >= 0) {
+    if (md5(input + i.toString()).slice(0, 6) === '000000') {
+        console.log(i);
+        break;
     }
+    i += 1;
 }
