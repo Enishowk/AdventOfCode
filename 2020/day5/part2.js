a4192e336e5657e3389780b1859c37d73409ba87
Enishowk <kenshineuh@gmail.com>
2020-12-05 23:08:24 +0100

    2020: day5

---
+++ 2020/day5/part2.js
@@ -0,0 +1,43 @@
+const inputs = require('fs')
+    .readFileSync(`${__dirname}/input.txt`, 'utf8')
+    .split(/\r?\n/);
+
+function decode(binary) {
+    let minRow = 0;
+    let maxRow = 127;
+    for (let i = 0; i < binary.length - 3; i += 1) {
+        const letter = binary[i];
+        if (letter === 'F') {
+            maxRow = Math.floor((maxRow + minRow) / 2);
+        }
+        if (letter === 'B') {
+            minRow = Math.floor((maxRow + minRow) / 2) + 1;
+        }
+    }
+
+    let left = 0;
+    let right = 7;
+    for (let i = 7; i < binary.length; i += 1) {
+        const letter = binary[i];
+        if (letter === 'L') {
+            right = Math.floor((right + left) / 2);
+        }
+        if (letter === 'R') {
+            left = Math.floor((right + left) / 2) + 1;
+        }
+    }
+
+    return minRow * 8 + left;
+}
+
+const allSeatsId = [];
+inputs.forEach(binary => {
+    allSeatsId.push(decode(binary));
+});
+allSeatsId.sort((a, b) => a - b);
+
+for (let i = allSeatsId[0]; i <= allSeatsId[allSeatsId.length - 1]; i += 1) {
+    if (!allSeatsId.includes(i)) {
+        console.log(i);
+    }
+}
