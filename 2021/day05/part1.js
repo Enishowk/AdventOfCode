b184499cfffc3087961ac8d1fb103bd30ef1a7e3
Enishowk <kenshineuh@gmail.com>
2021-12-05 23:38:26 +0100

    2021: day05

---
+++ 2021/day05/part1.js
@@ -0,0 +1,39 @@
+const inputs = require("fs")
+    .readFileSync(`${__dirname}/input.txt`, "utf8")
+    .split("\n")
+    .map((input) => input.split("->").map((input) => input.trim()));
+
+const GRID = Array.from({ length: 1000 }, () =>
+    Array.from({ length: 1000 }, () => ".")
+);
+
+const transformPoint = (x, y) => {
+    if (GRID[y][x] === ".") {
+        GRID[y][x] = 1;
+    } else if (GRID[y][x] === 1) {
+        GRID[y][x] += 1;
+    }
+};
+
+for (const line of inputs) {
+    const [x1y1, x2y2] = line;
+    const [x1, y1] = x1y1.split(",").map(Number);
+    const [x2, y2] = x2y2.split(",").map(Number);
+
+    if (x1 === x2) {
+        for (let i = 0; i <= Math.abs(y2 - y1); i += 1) {
+            transformPoint(x1, y1 < y2 ? y1 + i : y1 - i);
+        }
+    } else if (y1 === y2) {
+        for (let i = 0; i <= Math.abs(x2 - x1); i += 1) {
+            transformPoint(x1 < x2 ? x1 + i : x1 - i, y1);
+        }
+    }
+}
+
+let sum = 0;
+for (const line of GRID) {
+    sum += line.filter((element) => element === 2).length;
+}
+
+console.log(sum);
